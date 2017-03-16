c17a8b8e605738425d5a4935f9f7b2873aaaf0dd
ZJ <evillove0317@gmail.com>
2017-03-16 23:08:38 +0800

    add files

---
+++ NTHU104/Models/jobtable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webtesting.Models
+{
+    public class jobtable
+    {
+        private string jobcat;
+        private string jobname;
+        private string location;
+        private int years;
+        private string date;
+        private string postdate;
+
+        public jobtable() { }
+
+        public jobtable(string jc, string jn, string lo, int y, string _date, string pd)
+        {
+            this.jobcat = jc;
+            this.jobname = jn;
+            this.location = lo;
+            this.years = y;
+            this.date = _date;
+            this.postdate = pd;
+        }
+
+        public string Jobcat
+        {
+            get { return jobcat; }
+            set { jobcat = value; }
+        }
+
+        public string Jobname
+        {
+            get { return jobname; }
+            set { jobname = value; }
+        }
+
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public int Years
+        {
+            get { return years; }
+            set { years = value; }
+        }
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public string Postdate
+        {
+            get { return postdate; }
+            set { postdate = value; }
+        }
+    }
+}
---
+++ NTHU104/ws.asmx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+using webtesting.Models;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
+namespace webtesting
+{
+    /// <summary>
+    ///ws 的摘要描述
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允許使用 ASP.NET AJAX 從指令碼呼叫此 Web 服務，請取消註解下列一行。
+    [System.Web.Script.Services.ScriptService]
+    public class ws : System.Web.Services.WebService
+    {
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public void getTestJobData()
+        {
+            List<jobtable> result = new List<jobtable>();
+            result.Add(new jobtable("證券", "櫃台行員", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("金融", "櫃台行員", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("工程", "工程師", "高雄", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("證券", "櫃台行員", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("證券", "業務", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("工程", "工程師", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("證券", "業務", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("金融", "櫃台行員", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("工程", "工程師", "高雄", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("證券", "櫃台行員", "台中", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("金融", "櫃台行員", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("證券", "櫃台行員", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("工程", "工程師", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("證券", "櫃台行員", "高雄", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("金融", "業務", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("證券", "櫃台行員", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("工程", "工程師", "高雄", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("證券", "櫃台行員", "台中", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("金融", "業務", "台北", 3, "2017/03/10", "2017/05/10"));
+            result.Add(new jobtable("工程", "工程師", "台中", 3, "2017/03/10", "2017/05/10"));
+
+            //string resp = new JavaScriptSerializer().Serialize(result);
+
+            Context.Response.Clear();
+            Context.Response.ContentType = "application/json";
+            Context.Response.Write(JsonConvert.SerializeObject(result));
+        }
+    }
+}
